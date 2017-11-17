ec77176c00408e930de09b730207095b7cc07689
Sourabh Niyogi <sourabh@wolk.com>
2017-11-16 18:41:53 -0800

    Mayumi : add comments

--- src/github.com/ethereum/go-ethereum/swarmdb/database/interface.go
+++ src/github.com/ethereum/go-ethereum/swarmdb/database/interface.go
@@ -1,7 +1,9 @@
 package swarmdb
 
 type Database interface{
+// Insert + Update
 	Put(key []byte, value []byte) error
+// Insert only 
 	Insert(key []byte, value []byte) error
 	Get(key []byte)([]byte, error)
 	Delete(key []byte) error
@@ -11,5 +13,6 @@ type Database interface{
 
 type Batch interface{
 	Put(key []byte, value []byte) error
+// same as flush
 	Execute()error
 }
