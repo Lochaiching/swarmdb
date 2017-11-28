23b11b4202f1db1b74a1f98e89e9a5278750512c
Sourabh Niyogi <sourabh@wolk.com>
2017-11-27 22:49:56 -0800

    Mayumi: interface by Sourbh

--- src/github.com/ethereum/go-ethereum/swarmdb/database/interface.go
+++ src/github.com/ethereum/go-ethereum/swarmdb/database/interface.go
@@ -1,20 +1,55 @@
 package swarmdb
 
-type Database interface{
-// Insert + Update
-	Put(key []byte, value []byte) error
-// Insert only 
-	Insert(key []byte, value []byte) error
-	Get(key []byte)([]byte, error)
-	Delete(key []byte) error
-	Close()
-//	NewBatch() Batch
+type Database interface {
+	// Open: reads in root hashid from ENS
+	// Possible Errors: TableNotExistError, NetworkError
+	Open(owner string, tableName string, columnName string) (bool, error)
+
+	// Insert: adds key-value pair (value is an entire recrod)
+	// ok - returns true if new key added
+	// Possible Errors: KeySizeError, ValueSizeError, DuplicateKeyError, NetworkError, BufferOverflowError
+	Insert(key []byte, value []byte) (bool, error)
+
+	// Put -- inserts/updates key-value pair (value is an entire record)
+	// ok - returns true if new key added
+	// Possible Errors: KeySizeError, ValueSizeError, NetworkError, BufferOverflowError
+	Put(key []byte, value []byte) (bool, error)
+
+	// Get - gets value of key (value is an entire record)
+	// ok - returns true if key found, false if not found
+	// Possible errors: KeySizeError, NetworkError
+	Get(key []byte) ([]byte, bool, error)
+
+	// Delete - deletes key
+	// ok - returns true if key found, false if not found
+	// Possible errors: KeySizeError, NetworkError, BufferOverflowError
+	Delete(key []byte) (bool, error)
+
+	// Start/Flush - any buffered updates will be flushed to SWARM on FlushBuffer
+	// ok - returns true if buffer started / flushed
+	// Possible errors: NoBufferError, NetworkError
+	StartBuffer() (bool, error)
+	FlushBuffer() (bool, error)
+
+	// Close - if buffering, then will flush buffer
+	// ok - returns true if operation successful
+	// Possible errors: NetworkError
+	Close() (bool, error)
+
+	// prints what is in memory
+	Print()
 }
 
-/*
-type Batch interface{
-	Put(key []byte, value []byte) error
-// same as flush
-	Execute()error
+type OrderedDatabase interface {
+	Database
+
+	// Seek -- moves cursor to key k
+	// ok - returns true if key found, false if not found
+	// Possible errors: KeySizeError, NetworkError
+	Seek(k []byte /*K*/) (e OrderedDatabaseCursor, ok bool, err error)
 }
-*/
+
+type OrderedDatabaseCursor interface {
+	Next() (k []byte /*K*/, v []byte /*V*/, err error)
+	Prev() (k []byte /*K*/, v []byte /*V*/, err error)
+}
