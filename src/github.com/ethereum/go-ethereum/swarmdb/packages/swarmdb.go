004370ce0828a80a490fb68283927b2754de9554
Sourabh Niyogi <sourabh@wolk.com>
2017-11-28 12:28:32 -0800

    alina: packages/swarmdb.go

--- src/github.com/ethereum/go-ethereum/swarmdb/packages/swarmdb.go
+++ src/github.com/ethereum/go-ethereum/swarmdb/packages/swarmdb.go
@@ -20,6 +20,7 @@ import (
 	   "github.com/ethereum/go-ethereum/accounts"
 	   "github.com/ethereum/go-ethereum/crypto"
 	*/
+	//"bytes"
 	"crypto/sha256"
 	"fmt"
 	"net/http"
@@ -28,9 +29,10 @@ import (
 
 	"github.com/ethereum/go-ethereum/log"
 	api "github.com/ethereum/go-ethereum/swarm/api"
-	"github.com/robertkrimen/otto"
+	//"github.com/robertkrimen/otto"
 	//"github.com/ethereum/go-ethereum/swarm/storage"
 	//"github.com/robertkrimen/otto/repl"
+	"encoding/json"
 	"github.com/xwb1989/sqlparser"
 )
 
@@ -50,46 +52,113 @@ type Request struct {
 func SWARMDB_createTable(tbl_name string, column string, primary bool, index string) (succ bool) {
 	fmt.Printf("swarmdb.SWARMDB_createTable(%v, column: %v primary: %v index: %v)\n", tbl_name, column, primary, index)
 	// RODNEY/MAYUMI: CONNECT TO dispatch.go -- create table descriptor (in LocalDB + ENS), ...
+	switch index {
+	case "kademlia":
+		//fill in
+	case "hash":
+		//fill in
+	case "btree":
+		//fill in
+	}
+
 	return true
 }
 
-//func SWARMDB_query(sql string) (rec otto.Value) {
-func SWARMDB_query(sql string) (jsonarray []string, err error) {
-	// ALINA: FIGURE OUT HOW AN *** ARRAY ***  of JSON OBJECTS SHOULD BE RETURNED
-	// fmt.Printf("sql is: %s\n", sql)
+//data should be a pointer not actual structure
+func SWARMDB_query(sql string) (data []string, err error) {
+
+	//parse sql
 	stmt, err := sqlparser.Parse(sql)
 	if err != nil {
 		fmt.Printf("sqlparser.Parse err: %v\n", err)
-		//return otto.Value{}
-		return jsonarray, err
+		return data, err
 	}
-	fmt.Printf("swarmdb.SWARMDB_query(%s, ", sqlparser.String(stmt.(*sqlparser.Select).From))
-
-	for i, e := range stmt.(*sqlparser.Select).SelectExprs {
-		fmt.Printf(" field %d: %+v", i, sqlparser.String(e)) // stmt.(*sqlparser.Select).SelectExprs)
+	node := stmt.(*sqlparser.Select)
+	//for i, e := range node.SelectExprs {
+	//	fmt.Printf("FIELD %d: %+v\n", i, sqlparser.String(e)) // stmt.(*sqlparser.Select).SelectExprs)
+	//}
+	//really should only have 1 table
+	//for i, e := range node.From {
+	//	fmt.Printf("FROM %d: %s \n", i, sqlparser.String(e))
+	//}
+	if node.From == nil {
+		return data, fmt.Errorf("no table specified")
 	}
-	fmt.Printf(")\n")
-	//return otto.Value{}
 
-	//pretending this is the solution to whatever the query puts out...
-	jsonarray = append(jsonarray, `{ "email": "rodney@wolk.com", "name": "Rodney", "age": 38 }`)
-	jsonarray = append(jsonarray, `{ "email": "alina@wolk.com", "name": "Alina", "age": 35 }`)
-	return jsonarray, nil
+	//get table
+	//data, primarykeycol = GET(node.From[0])  //not sure a primary key col is a feature we'll have
+	//pretending this is the solution to whatever the query puts out... (i.e. the whole contacts table)
+	var dataget []string
+	dataget = append(dataget, `{ "email": "rodney@wolk.com", "name": "Rodney", "age": 38 }`)
+	dataget = append(dataget, `{ "email": "alina@wolk.com", "name": "Alina", "age": 35 }`)
+	dataget = append(dataget, `{ "email": "who@wolk.com", "name": "Who", "age": 38 }`)
+	//pretending it also turns out a primary key col:
+	//primarykeycol := "email"
+
+	if node.Where != nil {
+		fmt.Printf("WHERE: %s \n", sqlparser.String(node.Where))
+		readable(node.Where.Expr)
+
+		switch n := node.Where.Expr.(type) {
+		case *sqlparser.OrExpr:
+			left := strings.Split(sqlparser.String(n.Left), "=")
+			right := strings.Split(sqlparser.String(n.Right), "=")
+			for _, record := range dataget {
+				rmap := make(map[string]interface{})
+				if err := json.Unmarshal([]byte(record), &rmap); err != nil {
+					return data, err
+				}
+				if (rmap[left[0]] == left[1]) || rmap[right[0]] == right[1] {
+					data = append(data, record)
+				}
+
+			}
+		case *sqlparser.AndExpr:
+			left := strings.Split(sqlparser.String(n.Left), "=")
+			right := strings.Split(sqlparser.String(n.Right), "=")
+			for _, record := range dataget {
+				rmap := make(map[string]interface{})
+				if err := json.Unmarshal([]byte(record), &rmap); err != nil {
+					return data, err
+				}
+				if (rmap[left[0]] == left[1]) && rmap[right[0]] == right[1] {
+					data = append(data, record)
+				}
+			}
+		case *sqlparser.IsExpr:
+
+		}
+	}
+
+	//fmt.Printf("HAVING: %s \n", sqlparser.String(node.Having))
+
+	//fmt.Printf("GROUPBY: %s \n", sqlparser.String(node.GroupBy))
+
+	//fmt.Printf("ORDER BY: %s \n", sqlparser.String(node.OrderBy))
+
+	//fmt.Printf(")\n")
+
+	return data, nil
 }
 
-func SWARMDB_add(tbl_name string, rec *otto.Object) (succ bool, err error) {
-	// RODNEY/MAYUMI: CONNECT TO dispatch.go -- get table descriptor, get primary key's index type, ...
-	fmt.Printf("swarmdb.SWARMDB_add(%s, ", tbl_name)
-	for _, k := range rec.Keys() {
-		v, _ := rec.Get(k)
-		fmt.Printf(" key: %s value: %s", k, v)
+func SWARMDB_add(tablename string, record map[string]interface{}) (success bool, err error) {
+
+	fmt.Printf("swarmdb.SWARMDB_add(%s, %+v)\n", tablename, record)
+	for key, _ := range record {
+		switch key {
+		case "email":
+			// fill in
+		case "name":
+			// fill in
+		case "age":
+			// fill in
+
+		}
 	}
-	fmt.Printf(")\n")
 	return true, nil
 }
 
-func SWARMDB_get(tbl_name string, id string) (json string, err error) {
-	// ALINA: FIGURE OUT HOW A JSON OBJECT SHOULD BE RETURNED
+func SWARMDB_get(tbl_name string, id string) (jsonrecord string, err error) {
 
 	// RODNEY/MAYUMI: CONNECT TO dispatch.go
 	// get table descriptor, and based on the primary key's index, call dispatch.go
@@ -97,19 +166,26 @@ func SWARMDB_get(tbl_name string, id string) (json string, err error) {
 
 	index := GetColumnDesc("dummy", tbl_name, id)
 
-	if index == "kademlia" {
-		SwarmDbDownloadKademlia("0x728781E75735dc0962Df3a51d7Ef47E798A7107E", tbl_name, id)
-	} else if index == "hash" {
-		//SwarmDbDownloadKademlia( "dummy", tbl_name, id )
-	} else if index == "btree" {
-		//SwarmDbDownloadKademlia( "dummy", tbl_name, id )
-	}
+	record := make(map[string]interface{})
+	switch index {
+	case "kademlia":
+		record, err = SwarmDbDownloadKademlia("0x728781E75735dc0962Df3a51d7Ef47E798A7107E", tbl_name, id)
+	case "hash":
+		//record, err = SwarmDbDownloadKademlia( "dummy", tbl_name, id )
 
+	case "btree":
+		//record, err = SwarmDbDownloadKademlia( "dummy", tbl_name, id )
+	}
+	jr, _ := json.Marshal(record)
+	jsonrecord = string(jr)
+	if err != nil {
+		return jsonrecord, err
+	}
 	//let's say this is the answer out of the swarmdb: (tbl_name: contacts, id: rodeny@wolk.com)
-	json = `{ "email": "rodney@wolk.com", "name": "Rodney", "age": 38 }`
+	jsonrecord = `{ "email": "rodney@wolk.com", "name": "Rodney", "age": 38 }`
 
 	//return rec
-	return json, nil
+	return jsonrecord, nil
 }
 
 func GetColumnDesc(owner string, table string, column string) (index string) {
@@ -145,7 +221,7 @@ func SwarmDbUploadKademlia(owner string, table string, key string, content strin
 	*/
 }
 
-func SwarmDbDownloadKademlia(owner string, table string, id string) {
+func SwarmDbDownloadKademlia(owner string, table string, id string) (record map[string]interface{}, err error) {
 	/*
 	           keylen := 64 ///////..........
 	           dummy := bytes.Repeat([]byte("Z"), keylen)
@@ -158,7 +234,7 @@ func SwarmDbDownloadKademlia(owner string, table string, id string) {
 	           log.Debug(fmt.Sprintf("In HandleGetDB prefix [%v] dummy %v newkeybase %v key %v", contentPrefix, dummy, newkeybase, key))
 
 	   	fmt.Println("Key: ", key)
-	           contentReader := swarmdbApi.Retrieve(key)
+	           contentReader := api.Retrieve(key) //swarmdbapi.
 	           if _, err := contentReader.Size(nil); err != nil {
 	                   log.Debug("key not found %s: %s", key, err)
 	                   //http.NotFound(w, &r.Request)
@@ -179,6 +255,8 @@ func SwarmDbDownloadKademlia(owner string, table string, id string) {
 
 	           //decrypted_reader := DecryptData(encryptedContentBytes)
 	*/
+	return record, err
+
 }
 
 /*
@@ -209,4 +287,23 @@ func EncryptData( data []byte ) []byte {
 */
 func logDebug(format string, v ...interface{}) {
 	log.Debug(fmt.Sprintf("[SWARMDB] HTTP: "+format, v...))
+}
+
+//func andOperation(data ) {
+//}
+
+func readable(node sqlparser.Expr) string {
+	switch node := node.(type) {
+	case *sqlparser.OrExpr:
+		return fmt.Sprintf("(%s or %s)", readable(node.Left), readable(node.Right))
+	case *sqlparser.AndExpr:
+		//fmt.Printf("got to AND expr (%s and %s)", readable(node.Left), readable(node.Right))
+		return fmt.Sprintf("got to AND expr (%s and %s)", readable(node.Left), readable(node.Right))
+	case *sqlparser.BinaryExpr:
+		return fmt.Sprintf("(%s %s %s)", readable(node.Left), node.Operator, readable(node.Right))
+	case *sqlparser.IsExpr:
+		return fmt.Sprintf("(%s %s)", readable(node.Expr), node.Operator)
+	default:
+		return sqlparser.String(node)
+	}
 }
