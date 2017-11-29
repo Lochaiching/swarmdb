39a1abff90ffd3a0c22ba89ce7304625a59bb20c
Sourabh Niyogi <sourabh@wolk.com>
2017-11-29 14:30:37 -0800

    ROdney: swarmtcpip stubs

--- src/github.com/ethereum/go-ethereum/swarm/api/tcpip/server.go
+++ src/github.com/ethereum/go-ethereum/swarm/api/tcpip/server.go
@@ -2,11 +2,13 @@ package tcpip
 
 import (
 //	"io"
+	"encoding/gob"
+	"fmt"
 	"bufio"
-	"log"
 	"net"
 	"strings"
 	"sync"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/swarmdb/database"
 )
 
@@ -22,6 +24,14 @@ type ServerConfig struct{
 	Port	string
 }
 
+type HandlerFunc func(map[string]interface{})
+
+type funcCall struct {
+                FuncName string
+                Args     map[string]interface{}
+}
+var registeredHandlers map[string]HandlerFunc
+
 func NewServer(db *swarmdb.SwarmDB, l  net.Listener)(*Server){
 	sv := new(Server)
 	sv.swarmdb = db
@@ -29,26 +39,46 @@ func NewServer(db *swarmdb.SwarmDB, l  net.Listener)(*Server){
 	return sv
 }
 
-func StartTCPServer(swarmdb *swarmdb.SwarmDB, config *ServerConfig) {
+func StartTCPServer(swarmdb *swarmdb.SwarmDB) {
+//func StartTCPServer(swarmdb *swarmdb.SwarmDB, config *ServerConfig) {
 	//listen, err := net.Listen("tcp", config.Port)
         l, err := net.Listen("tcp", ":2000")
         
 	svr := NewServer(swarmdb, l)
 	if err != nil {
-		log.Fatal(err)
+		//log.Fatal(err)
 	}
 	//defer svr.listener.Close()
+
+        registeredHandlers = make(map[string]HandlerFunc)
+        registeredHandlers["TcpipDispatch"] = TcpipDispatch
 
 	for{
 		conn, err := svr.listener.Accept()
 		if err != nil {
-			log.Fatal(err)
+			//log.Fatal(err)
 		}
 	//defer svr.listener.Close()
-    		message, _ := bufio.NewReader(conn).ReadString('\n')
+    		tcpipReader := bufio.NewReader(conn)
+		message, _ := tcpipReader.ReadString('\n')
 		newmessage := strings.ToUpper(message)
     		conn.Write([]byte(newmessage + "\n"))
+                var data funcCall
+                dec := gob.NewDecoder(tcpipReader)
+                err = dec.Decode(&data)
+                log.Debug(fmt.Sprintf("[TCPIP] GOB Data: [%v]", data))
+                if err != nil {
+                        log.Debug(fmt.Sprintf("[TCPIP] Error decoding GOB data:", err))
+                        return 
+                }
+                log.Debug(fmt.Sprintf("[TCPIP] Trying to call [%v] ", data.FuncName))
+                log.Debug(fmt.Sprintf("[TCPIP] Trying to call [%v] ", data.Args))
+                registeredHandlers[data.FuncName](data.Args)
 	}
+}
+
+func TcpipDispatch(args map[string]interface{}) {
+        log.Debug(fmt.Sprintf("Printing Args from TCPIP_Printer: \n%#v\n", args))
 }
 
 func (svr *Server) HandleNewConnection(username, password string)(string, error){
