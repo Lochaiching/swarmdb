dc54460a3c54d433372fa3d966880a1896f77922
Yaron <yaron@wolk.com>
2017-12-07 14:45:13 -0800

    Yaron: sgx

---
+++ sgx/sgx-unseal/App/App.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+#include "Enclave_u.h"
+#include "sgx_urts.h"
+#include "sgx_utils/sgx_utils.h"
+
+int main(int argc, char const *argv[]) {
+    return 0;
+}
+
