fe9066f6b19238db018dafc403347f0aa1b903eb
Michael Chung <michael@wolk.com>
2017-12-08 19:17:26 -0800

    Michael: load existing DBchunkstore

--- src/github.com/ethereum/go-ethereum/swarm/storage/dbchunkstore.go
+++ src/github.com/ethereum/go-ethereum/swarm/storage/dbchunkstore.go
@@ -61,6 +61,7 @@ type netstatFile struct {
 	WalletAddress string
 	Claims        map[string]string
 	LaunchDT      time.Time
+    LWriteDT      time.Time
 	LogDT         time.Time
 }
 
@@ -89,11 +90,13 @@ func (self *DBChunkstore) UnmarshalJSON(data []byte) error {
 	var file netstatFile
 	err := json.Unmarshal(data, &file)
 	if err != nil {
+        fmt.Println(err)
 		return err
 	}
 
 	self.launchDT = file.LaunchDT
 	self.logDT = file.LogDT
+    self.lwriteDT = file.LWriteDT
 	self.nodeid = file.NodeID
 	self.farmer = ethcommon.HexToAddress(file.WalletAddress)
 
@@ -188,6 +191,46 @@ func NewDBChunkStore(path string) (self *DBChunkstore, err error) {
 	return
 }
 
+func LoadDBChunkStore(path string) (self *DBChunkstore, err error) {
+    var data []byte
+    devaultDBPath := "netstat.json"
+    data, err = ioutil.ReadFile(devaultDBPath)
+    if err != nil {
+        fmt.Printf("Error in Loading netStat from %s.. generating new Log instead\n", devaultDBPath)
+        self, _ = NewDBChunkStore(path)
+        return self, nil
+    
+    }
+
+    self = &DBChunkstore{}
+    err = json.Unmarshal(data, self)
+    if err != nil {
+        fmt.Printf("Error in Parsing netStat new Log created\n => %s\n", err)
+        self, _ = NewDBChunkStore(path)
+        return self, nil
+    }
+
+    db, err := sql.Open("sqlite3", path)
+    if err != nil {
+        return nil, err 
+    }   
+    if db == nil {
+        return nil, err 
+    }
+
+    km, errKm := keymanager.NewKeyManager("/tmp/blah")
+    if errKm != nil {
+        fmt.Printf("Error Creating KeyManager")
+        return nil, err
+    }
+
+    self.db = db
+    self.km =  &km
+    self.filepath = path
+    self.statpath = devaultDBPath    
+    return
+}
+
 func (self *DBChunkstore) StoreKChunk(k []byte, v []byte) (err error) {
 	if len(v) < minChunkSize {
 		return fmt.Errorf("chunk too small") // should be improved
@@ -245,6 +288,7 @@ func (self *DBChunkstore) StoreChunk(v []byte) (k []byte, err error) {
 		return k, err2
 	}
 	stmt.Close()
+    self.lwriteDT = time.Now()
 	return k, nil
 }
 
--- src/github.com/ethereum/go-ethereum/swarm/storage/netStat_test.go
+++ src/github.com/ethereum/go-ethereum/swarm/storage/netStat_test.go
@@ -9,7 +9,8 @@ import (
 
 var (
     testDBPath = "chunks.db"
-    chunkTotal = 1000    
+    chunkTotal = 1000
+        
 )
 
 
@@ -68,4 +69,57 @@ func TestDBChunkStore(t *testing.T) {
 
 }
 
+func TestLoadDBChunkStore (t *testing.T) {
 
+    //Opening existing DB
+    store, err := storage.LoadDBChunkStore(testDBPath)
+    if err != nil {
+        t.Fatal("[FAILURE] to open existing DBChunkStore\n")
+    }else{
+        fmt.Printf("[SUCCESS] open exsisting DBChunkStore\n")
+    }
+
+
+    t.Run("EWrite", func(t *testing.T){
+        //Simulate chunk writes w/ n chunkTotal
+        for j := 0; j < chunkTotal; j++ {
+            simdata := make([]byte, 4096)
+            tmp := fmt.Sprintf("%s%d", "randombytes",j)
+            copy(simdata,tmp)
+            simh, err := store.StoreChunk(simdata)
+            if err != nil {
+                t.Fatal("[FAILURE] writting record #%v [%x] => %v\n",j, simh, string(simdata[:]) )
+            }else if j % 50 == 0 {
+                fmt.Printf("Generating record [%x] => %v ... ", simh, string(simdata[:]) )
+                fmt.Printf("[SUCCESS] writing #%v chunk to %v\n", j, testDBPath)
+            }
+        }
+    })
+
+    t.Run("EScan", func(t *testing.T) {
+        err := store.ScanAll()
+        if err != nil {
+            t.Fatal("[FAILURE] ScanAll Error\n")
+        }else {
+            fmt.Printf("[SUCCESS] ScanAll Operation\n")
+        }
+    })
+
+    t.Run("EStat", func(t *testing.T) {
+        res, err := store.GetChunkStat()
+        if err != nil {
+            t.Fatal("[FAILURE] netStat Retrieval Error\n")
+        }else {
+            fmt.Printf("[SUCCESS] netStat optput: %s\n", res)
+        }
+    })
+
+    t.Run("ESave", func(t *testing.T) {
+        err := store.Save()
+        if err != nil {
+            t.Fatal("[FAILURE] unable to generate netStat json\n")
+        }else {
+            fmt.Printf("[SUCCESS] netStat stored in persisted files\n")
+        }
+    })
+}
