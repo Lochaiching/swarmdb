33bca11b3e4dcae2543b420be729815da80c684e
Sourabh Niyogi <sourabh@wolk.com>
2017-12-12 16:16:26 -0800

    Rodney: bid, enc, rep

--- src/github.com/ethereum/go-ethereum/swarmdb/types.go
+++ src/github.com/ethereum/go-ethereum/swarmdb/types.go
@@ -72,7 +72,9 @@ type RequestOption struct {
 	RequestType string   `json:"requesttype"` //"OpenConnection, Insert, Get, Put, etc"
 	Owner       string   `json:"owner,omitempty"`
 	Table       string   `json:"table,omitempty"` //"contacts"
-	Index       string   `json:"index,omitempty"`
+	Encrypted   bool     `json:"encrypted,omitempty"`
+	Bid	    float64  `json:"bid,omitempty"`
+	Replication int64    `json:"replication,omitempty"`
 	Key         string   `json:"key,omitempty"`   //value of the key, like "rodney@wolk.com"
 	Value       string   `json:"value,omitempty"` //value of val, usually the whole json record
 	Columns     []Column `json:"columns",omitempty"`
