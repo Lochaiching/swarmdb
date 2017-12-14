bcb82af575dcfb0e8673eb3fbb0c8f29200fe2b8
Yaron <yaron@wolk.com>
2017-12-14 14:39:12 -0800

    Yaron: wolk-crypto

--- sgx/wolk-crypto/cgo.go
+++ sgx/wolk-crypto/cgo.go
@@ -3,12 +3,13 @@ package main
 /*
 #include "./App/TEE.h"
 #cgo LDFLAGS: -I./App -L. -ltee 
+#include <stdlib.h>
 */
 import "C"
 
 import (
 	"fmt"
-	//"unsafe"
+	"unsafe"
 )
 
 //export test
@@ -19,7 +20,7 @@ func test() {
 func main() {
 
     cstr := C.CString("TestThisSGX")
-    // defer C.free(unsafe.Pointer(cstr))
+    defer C.free(unsafe.Pointer(cstr))
     cString := C.getSha256(cstr)          // hash: 18497686A320B7DA753F7E18C58C4F2E18089D5816FDE68858878D22C8237E36
     gostr := C.GoString(cString)
     fmt.Println("Received hash (string) from C: " + gostr)
@@ -27,4 +28,4 @@ func main() {
     //test()
 }
 
- 
+// http://geekwentfreak.com/posts/golang/cgo_pass_receive_string_c/ 
