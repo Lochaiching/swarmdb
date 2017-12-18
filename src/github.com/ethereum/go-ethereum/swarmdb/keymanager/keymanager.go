fb36cb037fe033e6de266a8218761e9ab49eab79
Sourabh Niyogi <sourabh@wolk.com>
2017-12-17 16:19:11 -0800

    Sourabh: keystore ops

--- src/github.com/ethereum/go-ethereum/swarmdb/keymanager/keymanager.go
+++ src/github.com/ethereum/go-ethereum/swarmdb/keymanager/keymanager.go
@@ -1,112 +1,125 @@
 package keymanager
 
 import (
-	"crypto/sha256"
+	"bytes"
 	"fmt"
 	"github.com/ethereum/go-ethereum/accounts"
+"golang.org/x/crypto/nacl/box"
+
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"golang.org/x/crypto/nacl/box"
-//	"hash"
-//	"io"
-	"io/ioutil"
+	"github.com/andreburgaud/crypt2go/ecb"
+	"github.com/andreburgaud/crypt2go/padding"
+	"crypto/aes"
+	"crypto/cipher"
+	// "os"
 )
 
 type KeyManager struct {
-	keystore *keystore.Key
-	pk       [32]byte
-	sk       [32]byte
+	OwnerAccount accounts.Account
+	OwnerAddress common.Address
+	keystore     *keystore.KeyStore
+	passphrase   string
+	pk           []byte
+	sk           []byte
+	publicK      [32]byte
+	secretK      [32]byte
+	aescipher    cipher.Block
 }
 
-func NewKeyManager() (keymgr KeyManager, err error) {
-	keystore, err := GetKeystore()
-	if err != nil {
-		return keymgr, err
-	} else {
-		keymgr.keystore = keystore
-		sk, pk := ExtractPKSK(keymgr.keystore)
-		copy(keymgr.sk[0:], sk)
-		copy(keymgr.pk[0:], pk)
-	}
-	return keymgr, nil
-}
+func NewKeyManager(path string, ownerAddress string, passphrase string) (keymgr KeyManager, err error) {
+	keymgr.keystore = keystore.NewKeyStore(path, keystore.StandardScryptN, keystore.StandardScryptP)
+	keymgr.passphrase = passphrase
+	keymgr.OwnerAddress = common.HexToAddress(ownerAddress)
 
-func GetKeystore() (keyWrapper *keystore.Key, err error) {
-	path := "/var/www/vhosts/sourabh/swarm.wolk.com/src/github.com/ethereum/go-ethereum/swarmdb/keymanager/"
-	ks := keystore.NewKeyStore(path, keystore.StandardScryptN, keystore.StandardScryptP)
-	var ks_accounts []accounts.Account //     type Account struct    in->   keystore/keystore.go
-	ks_accounts = ks.Accounts()
-	if len(ks_accounts) > 0 {
-		acc_url := ks_accounts[0].URL
-		acc_url_string := fmt.Sprintf("%s", acc_url)
-		filename := acc_url_string[11:] // /var/www/vhosts/data/keystore/UTC--2017-10-13T23-15-16.214744640Z--dc8a520a69157a7087f0b575644b8e454f462159
-		// Open the keystore file
-		keyJson, readErr := ioutil.ReadFile(filename)
-		if readErr != nil {
-			return keyWrapper, readErr
-		} else {
-			keyWrapper, keyErr := keystore.DecryptKey([]byte(keyJson), "h3r0c1ty!")
-			// fmt.Printf("KEYS: %v\n", keyWrapper)
-			if keyErr != nil {
-				return keyWrapper, keyErr
+	wallets := keymgr.keystore.Wallets()
+	for _, w := range wallets {
+		accounts := w.Accounts()
+		for _, a := range accounts {
+			if bytes.Compare(a.Address.Bytes(), keymgr.OwnerAddress.Bytes()) == 0 {
+				err := keymgr.keystore.Unlock(a, keymgr.passphrase)
+				_, k, err := keymgr.keystore.WgetDecryptedKey(a, passphrase)
+				if err != nil {
+					return keymgr, err
+				} else {
+					// fmt.Printf("SUCC: %v %v Address 0x%x\n", a, k, accounts[0].Address)
+					keymgr.OwnerAccount = a
+					keymgr.sk = crypto.FromECDSA(k.PrivateKey)
+					keymgr.pk = crypto.FromECDSAPub(&k.PrivateKey.PublicKey)
+					copy(keymgr.publicK[0:], keymgr.pk[0:])
+					copy(keymgr.secretK[0:], keymgr.sk[0:])
+
+					aescipher, errcip := aes.NewCipher(keymgr.sk) 
+					if errcip != nil {
+						return keymgr, err
+					}
+					keymgr.aescipher = aescipher
+				}
 			} else {
-				return keyWrapper, nil
+				// fmt.Printf("Keystore Not found %x\n%x\n", accounts[0].Address.Bytes(), keymgr.OwnerAddress.Bytes())
+				return keymgr, fmt.Errorf("No keystore file found %x", ownerAddress)
 			}
 		}
 	}
-	return keyWrapper, fmt.Errorf("No accounts found")
+
+	return keymgr, nil
 }
 
-func (self *KeyManager) SignMessage(msg string) (sig []byte, hashout []byte, err error) {
-	privateKey := self.keystore.PrivateKey
+func (self *KeyManager) GetPublicKey() []byte {
+	return self.pk // crypto.FromECDSAPub(signerKey)
+}
 
-	// The produced signature is in the [R || S || V] format where V is 0 or 1.
-	h256 := sha256.New()
-	h256.Write([]byte(msg))
-	hashout = h256.Sum(nil)
-
-	sig, err = crypto.Sign(hashout, privateKey)
+func (self *KeyManager) SignMessage(msg_hash []byte) (sig []byte, err error) {
+	sig, err = self.keystore.SignHash(self.OwnerAccount, msg_hash)
 	if err != nil {
-		return sig, hashout, err
+		return sig, err
 	}
-	return sig, hashout, err
+	return sig, nil
 }
 
-func ExtractPKSK(keyWrapper *keystore.Key) (acc_sk []byte, acc_pk []byte) {
-	var sk [32]byte
-	var pk [32]byte
-
-	acc_sk = crypto.FromECDSA(keyWrapper.PrivateKey)
-	acc_pk = crypto.FromECDSAPub(&keyWrapper.PrivateKey.PublicKey)
-	acc_pk = append(pk[:0], pk[1:]...)
-	// fun call elliptic.Marshal   add  ret[0] = 4 // uncompressed point
-	// pk:[]byte{0x4, 0x8d, 0x9b,
-	// need to remove the "ret[0] = 4" to get the pk
-
-	//secretkey := [32]byte{}
-	for i := range acc_sk {
-		sk[i] = acc_sk[i]
-		if i == 31 {
-			break
-		}
+func (self *KeyManager) VerifyMessage(msg_hash []byte, sig []byte) (verified bool, err error) {
+	pubKey, err := crypto.SigToPub(msg_hash, sig)
+	if err != nil {
+		return false, err
 	}
-
-	// crypto/nacl  box.Seal()  box.Open()   PublicKey is  type *[32]byte  so cut the account PublicKey from [64]byte to [32]byte
-	//publickey := [64]byte{}
-	//publickey := [32]byte{}
-	for i := range acc_pk {
-		pk[i] = acc_pk[i]
-		if i == 31 {
-			break
-		}
+	if !bytes.Equal(crypto.FromECDSAPub(pubKey), self.pk) {
+		return false, fmt.Errorf("signer mismatch: %x != %x", crypto.FromECDSAPub(pubKey), self.pk)
 	}
-	return acc_sk, acc_pk
+	return true, nil
+
 }
+
+func (self *KeyManager) DecryptDataAES(src []byte) (dst []byte, err error) {
+	mode := ecb.NewECBEncrypter(self.aescipher)
+	padder := padding.NewPkcs7Padding(mode.BlockSize())
+	dst, err = padder.Pad(src) // padd last block of plaintext if block size less than block cipher size
+	if err != nil {
+		return dst, err
+	}
+	dst = make([]byte, len(dst))
+	mode.CryptBlocks(dst, src)
+	return dst, err
+}
+
+func (self *KeyManager) EncryptDataAES(src []byte) (dst []byte, err error) {
+	mode := ecb.NewECBDecrypter(self.aescipher)
+	dst = make([]byte, len(src))
+	mode.CryptBlocks(dst, src)
+	padder := padding.NewPkcs7Padding(mode.BlockSize())
+	dst, err = padder.Unpad(dst) // unpad plaintext after decryption
+	if err != nil {
+		return dst, err
+	}
+	return dst, err
+}
+
 
 func (self *KeyManager) DecryptData(data []byte) []byte {
 	var decryptNonce [24]byte
 	copy(decryptNonce[:], data[:24])
-	decrypted, ok := box.Open(nil, data[24:], &decryptNonce, &self.pk, &self.sk)
+
+	decrypted, ok := box.Open(nil, data[24:], &decryptNonce, &self.publicK, &self.secretK)
 	if !ok {
 		panic("decryption error")
 	}
@@ -118,15 +131,7 @@ func (self *KeyManager) EncryptData(data []byte) []byte {
 	// fix required
 	nonce = [24]byte{4, 0, 50, 203, 12, 81, 11, 49, 236, 255, 155, 11, 101, 6, 97, 233, 94, 169, 107, 4, 37, 57, 106, 151}
 	msg := data //[]byte("Alas, poor Yorick! I knew him, Horatio")
-	encrypted := box.Seal(nonce[:], msg, &nonce, &self.pk, &self.sk)
+	encrypted := box.Seal(nonce[:], msg, &nonce, &self.publicK, &self.secretK)
 	return encrypted
 }
 
-/*
-func main() {
-	privateFile := "f.prv"
-	publicFile := "f.pub"
-	// privateKey, publicKey := GenKeys(privateFile, publicFile)
-	privateKey, publicKey := LoadKeys(privateFile, publicFile)
- }
-*/
--- src/github.com/ethereum/go-ethereum/swarmdb/keymanager/keymanager_test.go
+++ src/github.com/ethereum/go-ethereum/swarmdb/keymanager/keymanager_test.go
@@ -1,60 +1,94 @@
 package keymanager_test
 
 import (
-	"fmt"
 	"bytes"
-	"math/big"
-	"github.com/ethereum/go-ethereum/crypto"
+	"crypto/sha256"
+	"fmt"
+	// "github.com/ethereum/go-ethereum/crypto"
 	// "encoding/binary"
 	"github.com/ethereum/go-ethereum/swarmdb/keymanager"
 	"testing"
 )
 
-func TestSignMessage(t *testing.T) {
-	km, err := keymanager.NewKeyManager()
+const (
+	TEST_MSG             = "sourabh"
+	PATH                 = "/var/www/vhosts/sourabh/swarm.wolk.com/src/github.com/ethereum/go-ethereum/swarmdb/keymanager/"
+	WOLKSWARMDB_ADDRESS  = "b6d1561697854dfa502140c8e2128f4ca4015b59"
+	WOLKSWARMDB_PASSWORD = "h3r0c1ty!"
+)
+
+func TestSignVerifyMessage(t *testing.T) {
+	km, err := keymanager.NewKeyManager(PATH, WOLKSWARMDB_ADDRESS, WOLKSWARMDB_PASSWORD)
+
+	if err != nil {
+
+		t.Fatal("Failure to open KeyManager", err)
+	}
+	msg := "swarmdb"
+
+	h256 := sha256.New()
+	h256.Write([]byte(msg))
+	msg_hash := h256.Sum(nil)
+
+	sig, err := km.SignMessage(msg_hash)
+	if err != nil {
+		t.Fatal("sign err", err)
+	}
+
+	verified, err := km.VerifyMessage(msg_hash, sig)
+	if err != nil || !verified {
+		t.Fatal("verify err", err)
+	} else {
+		fmt.Printf("Verified signature %x\n", sig)
+	}
+}
+
+func failTestEncryptDecryptAES(t *testing.T) {
+	km, err := keymanager.NewKeyManager(PATH, WOLKSWARMDB_ADDRESS, WOLKSWARMDB_PASSWORD)
 	if err != nil {
 		t.Fatal("Failure to open KeyManager", err)
 	}
-	msg := "sourabh"
-	sig, h, err := km.SignMessage(msg)
-	if err != nil {
-		t.Fatal("sign err", err)
-	} else {
-		fmt.Printf("%x\n", sig)
-	}
-	
-	r := new(big.Int).SetBytes(sig[:32])
-	s := new(big.Int).SetBytes(sig[32:64])
-	v := sig[64]
 
-	a := crypto.ValidateSignatureValues(v, r, s, false)
-	if a {
-		fmt.Printf("SUCCESS\n")
-	} else {
-		t.Fatal("Failure to verify")
+	msg := "0123456789abcdef"
+	r := []byte(msg)
+
+	encData, err2 := km.EncryptDataAES(r)
+	if err2 != nil {
+		t.Fatal(err2)
 	}
-	p, err := crypto.Ecrecover(h, sig)
-	if err != nil {
-		t.Fatal("Failure to ecrecover")
-	}  else {
-		fmt.Printf("pubkey: %x\n", p)
+	decData, err3 := km.DecryptDataAES(encData)
+	if err3 != nil {
+		t.Fatal(err3)
+	}
+	a := bytes.Compare(decData, r)
+	if a != 0 {
+		fmt.Printf("Encrypted data is [%v][%x]", encData, encData)
+		fmt.Printf("Decrypted data is [%v][%s] => %d", decData, decData, a)
+		t.Fatal("Failure to decrypt")
+	} else {
+		fmt.Printf("Success %s\n", msg)
 	}
 
 }
 
-func aTestEncryptDecrypt(t *testing.T) {
-	km, err := keymanager.NewKeyManager()
+func TestEncryptDecrypt(t *testing.T) {
+	km, err := keymanager.NewKeyManager(PATH, WOLKSWARMDB_ADDRESS, WOLKSWARMDB_PASSWORD)
 	if err != nil {
 		t.Fatal("Failure to open KeyManager", err)
 	}
 
-	r := []byte("sourabh")
+	msg := "0123456789abcdef"
+	r := []byte(msg)
 
 	encData := km.EncryptData(r)
 	decData := km.DecryptData(encData)
-	a := bytes.Compare(decData, r) 
-
-	fmt.Printf("Encrypted data is [%v][%x]", encData, encData)
-	fmt.Printf("Decrypted data is [%v][%s] => %d", decData, decData, a)
+	a := bytes.Compare(decData, r)
+	if a != 0 {
+		fmt.Printf("Encrypted data is [%v][%x]", encData, encData)
+		fmt.Printf("Decrypted data is [%v][%s] => %d", decData, decData, a)
+		t.Fatal("Failure to decrypt")
+	} else {
+		fmt.Printf("Success %s\n", msg)
+	}
 
 }
