e3c67a5636b28245584e25137771a4bad4bccf02
Sourabh Niyogi <sourabh@wolk.com>
2017-12-19 15:20:44 -0800

    alina: comm queryoption chg

--- src/github.com/ethereum/go-ethereum/swarmdb/comm.go
+++ src/github.com/ethereum/go-ethereum/swarmdb/comm.go
@@ -16,18 +16,18 @@ type Column struct {
 
 //for passing request data from client to server
 type RequestOption struct {
-	RequestType string     `json:"requesttype"` //"OpenConnection, Insert, Get, Put, etc"
-	Owner       string     `json:"owner,omitempty"`
-	Table       string     `json:"table,omitempty"` //"contacts"
-	Encrypted   int        `json:"encrypted,omitempty"`
-	Bid         float64    `json:"bid,omitempty"`
-	Replication int        `json:"replication,omitempty"`
-	Key         string     `json:"key,omitempty"`   //value of the key, like "rodney@wolk.com"
-	Value       string     `json:"value,omitempty"` //value of val, usually the whole json record
-	Row         SWARMDBRow `json:"row,omitempty"`
-	Columns     []Column   `json:"columns,omitempty"`
-	RawQuery    string     `json:"rawquery,omitempty"` //"Select name, age from contacts where email = 'blah'"
-	Query       Query      `json:"query,omitempty"`    //Parsed query
+	RequestType string      `json:"requesttype"` //"OpenConnection, Insert, Get, Put, etc"
+	Owner       string      `json:"owner,omitempty"`
+	Table       string      `json:"table,omitempty"` //"contacts"
+	Encrypted   int         `json:"encrypted,omitempty"`
+	Bid         float64     `json:"bid,omitempty"`
+	Replication int         `json:"replication,omitempty"`
+	Key         string      `json:"key,omitempty"`   //value of the key, like "rodney@wolk.com"
+	Value       string      `json:"value,omitempty"` //value of val, usually the whole json record
+	Row         SWARMDBRow  `json:"row,omitempty"`
+	Columns     []Column    `json:"columns,omitempty"`
+	RawQuery    string      `json:"rawquery,omitempty"` //"Select name, age from contacts where email = 'blah'"
+	Query       QueryOption `json:"query,omitempty"`    //Parsed query
 }
 
 type SWARMDBConnection struct {
