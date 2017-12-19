c73765c2f619b668e946c97ab36dfdc8fbd721c2
Sourabh Niyogi <sourabh@wolk.com>
2017-12-19 08:04:27 -0800

    alina: types query update + gofmt

--- src/github.com/ethereum/go-ethereum/swarmdb/types.go
+++ src/github.com/ethereum/go-ethereum/swarmdb/types.go
@@ -46,8 +46,8 @@ type ENSSimulation struct {
 }
 
 type IncomingInfo struct {
-        Data    string
-        Address string
+	Data    string
+	Address string
 }
 
 type KademliaDB struct {
@@ -71,15 +71,15 @@ type SwarmDB struct {
 
 //for sql parsing
 type Query struct {
-	Type string //"Select" or "Insert" or "Update" probably should be an enum
-	RequestColumns []Column
-	Table string 
-	Where Where
+	Type      string //"Select" or "Insert" or "Update" probably should be an enum
+	Where     Where
+	Ascending int //1 true, 0 false (descending)
 }
+
 //for sql parsing
 type Where struct {
-	Left string
-	Right string
+	Left     string
+	Right    string
 	Operator string //sqlparser.ComparisonExpr.Operator; sqlparser.BinaryExpr.Operator; sqlparser.IsExpr.Operator; sqlparser.AndExpr.Operator, sqlparser.OrExpr.Operator
 }
 
