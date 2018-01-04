5fffed2b365a0a86e5cb618794cd95a8bcd7ac49
Sourabh Niyogi <sourabh@wolk.com>
2018-01-04 14:33:33 -0800

    alina: types Rows fcns modify

--- src/github.com/ethereum/go-ethereum/swarmdb/types.go
+++ src/github.com/ethereum/go-ethereum/swarmdb/types.go
@@ -281,7 +281,7 @@ func rowDataToJson(rows []Row) (string, error) {
 
 //gets only the specified Columns (column name and value) out of a single Row, returns as a Row with only the relevant data
 func filterRowByColumns(row *Row, columns []Column) (filteredRow Row) {
-	filteredRow.primaryKeyValue = row.primaryKeyValue
+	//filteredRow.primaryKeyValue = row.primaryKeyValue
 	filteredRow.cells = make(map[string]interface{})
 	for _, col := range columns {
 		if _, ok := row.cells[col.ColumnName]; ok {
