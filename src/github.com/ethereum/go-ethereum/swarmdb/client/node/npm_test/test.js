1ea34a4336e862619b41105462ad13e4c0e5699e
Bruce Han <bruce@wolk.com>
2018-01-23 17:36:54 -0800

    Bruce: swarmdb.js test file

--- src/github.com/ethereum/go-ethereum/swarmdb/client/node/npm_test/test.js
+++ src/github.com/ethereum/go-ethereum/swarmdb/client/node/npm_test/test.js
@@ -1,69 +1,52 @@
 var swarmdb = require("swarmdb.js");
 
 var connection = new swarmdb.createConnection({
-    host: "127.0.0.1",
-    port: 2000
+    host: "localhost",
+    port: 2001
 });
 
-var tableowner = "9982ad7bfbe62567287dafec879d20687e4b76f5";
+var tableowner = "ADDRESS_IN_YOUR_CONFIG_FILE";
 
 // create table
 var columns = [
-    { "indextype": 2, "columnname": "email", "columntype": 2, "primary": 0 },
-    { "indextype": 2, "columnname": "age", "columntype": 1, "primary": 1 }
+    { "indextype": 1, "columnname": "email", "columntype": 2, "primary": 1 },
+    { "indextype": 2, "columnname": "age", "columntype": 1, "primary": 0 }
 ];
-connection.createTable("test", tableowner, columns, function (err, result) {
+connection.createTable("test", columns, function (err, result) {
     if (err) throw err;
     console.log("create table response: " + result);
 });
 
-// // put
+// put
 connection.put("test", tableowner, [{"Cells": {"age":1,"email":"test001@wolk.com"}}], function (err, result) {
     if (err) throw err;
-    console.log("put response: " + result);
+    console.log("put response 1: " + result);
 });
 
 connection.put("test", tableowner, [{"Cells": {"age":2,"email":"test002@wolk.com"}}], function (err, result) {
     if (err) throw err;
-    console.log("put response: " + result);
+    console.log("put response 2: " + result);
 });
 
 connection.put("test", tableowner, [{"Cells": {"age":3,"email":"test003@wolk.com"}}], function (err, result) {
     if (err) throw err;
-    console.log("put response: " + result);
+    console.log("put response 3: " + result);
 });
 
 
 // get
-connection.get("test", tableowner, "2", function (err, result) {
+connection.get("test", tableowner, "test001@wolk.com", function (err, result) {
     if (err) throw err;
-    console.log("get response: " + result);
+    console.log("get response 1: " + result);
 });
-/*
+
 connection.get("test", tableowner, "test003@wolk.com", function (err, result) {
     if (err) throw err;
-    console.log("get response: " + result);
+    console.log("get response 2: " + result);
 });
 
 // query
-
 connection.query("select email, age from test where email = 'test001@wolk.com'", tableowner, function (err, result) {
     if (err) throw err;
-    console.log("query response: " + result);
-});
-
-connection.query("select email, age from test where email = 'test002@wolk.com'", tableowner, function (err, result) {
-    if (err) throw err;
-    console.log("query response: " + result);
-});
-*/
-
-connection.query("select email, age from test where age > 2", tableowner, function (err, result) {
-    if (err) throw err;
-    console.log("query response: " + result);
-});
-
-connection.query("select email, age from test where age = 3", tableowner, function (err, result) {
-    if (err) throw err;
-    console.log("query response: " + result);
+    console.log("query response 1: " + result);
 });
