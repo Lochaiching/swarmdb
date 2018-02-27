afa9747bbb11bf477e2ea0ac93e0cb2cf89afc6d
Rodney Witcher <rodney@wolk.com>
2018-02-27 13:05:01 -0800

    cleanup debug

--- src/github.com/ethereum/go-ethereum/swarmdb/server/wolkdb.go
+++ src/github.com/ethereum/go-ethereum/swarmdb/server/wolkdb.go
@@ -464,7 +464,7 @@ func (s *HTTPServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	default:
 	}
-	log.Debug("Logdata", logData)
+	log.Debug(fmt.Sprintf("Logdata", logData))
 	swReq, err := parsePath(r.URL.Path)
 	if err != nil {
 		retJson := buildErrorResp(err)
